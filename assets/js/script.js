5830d5566c45aa0c924fd2d305565e63c226aa48
khushbupatel05 <khushbupatel0009@gmail.com>
2025-04-17 22:01:53 +0530

    ui box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,70 @@
-let countryName = document.getElementById("countryName")
-let btnsubmit = document.getElementById("btn-submit")
+let countryNameInput = document.getElementById("countryName");
+let btnsubmit = document.getElementById("btn-submit");
 let answer = document.getElementById("answer");
 
+btnsubmit.addEventListener("click", function () {
+  let countryName = countryNameInput.value.trim();
+  answer.innerHTML = ""; 
 
-btnsubmit.addEventListener("click", function (){
-    let countryName  = document.getElementById("countryName").value;
+  if (countryName === "") {
+    Swal.fire({
+      position: "top",
+      icon: "error",
+      title: "Enter Sate Name",
+      showConfirmButton: false,
+      timer: 1500
+      
+    });
+    return;
+  }
 
-    const URL = `https://api.rootnet.in/covid19-in/stats/latest`;
-    fetch(URL)
-    .then((res)=> {
-        return res.json();
-    })
+  const URL = `https://api.rootnet.in/covid19-in/stats/latest`;
+
+  fetch(URL)
+    .then((res) => res.json())
     .then((res) => {
-        let states = res.data.regional;
+      let states = res.data.regional;
+      let found = false;
 
-        for(let i = 0; i < states.length; i++){
-            if(states[i].loc.toLowerCase() === countryName.toLowerCase()){
-                console.log(states[i])
-
-                answer.innerHTML +=
-                    "Total Confirmed: " + states[i].totalConfirmed + "<br>" +
-                    "Deaths: " + states[i].deaths + "<br>";
-                return;    
-            }
+      for (let i = 0; i < states.length; i++) {
+        if (states[i].loc.toLowerCase() === countryName.toLowerCase()) {
+            answer.innerHTML = `
+            <div class="stat-card confirmed">
+              <div class="stat-info">
+                <p class="stat-title">Total Confirmed</p>
+                <p class="stat-value">${states[i].totalConfirmed}</p>
+              </div>
+              <i class="bi bi-bar-chart-line fs-3"></i>
+            </div>
+            <div class="stat-card discharged">
+              <div class="stat-info">
+                <p class="stat-title">Discharged</p>
+                <p class="stat-value">${states[i].discharged}</p>
+              </div>
+             <i class="bi bi-person-check fs-3"></i>
+            </div>
+            <div class="stat-card deaths">
+              <div class="stat-info">
+                <p class="stat-title">Deaths</p>
+                <p class="stat-value">${states[i].deaths}</p>
+              </div>
+             <i class="bi bi-person-x fs-3"></i>
+            </div>
+          `;
+          found = true;
+          break;
         }
-    })
-});  
+      }
+
+      if (!found) {
+        Swal.fire({
+          position: "top",
+          icon: "error",
+          title: "State not found",
+          showConfirmButton: false,
+          timer: 1500
+          
+        });
+      }
+    });
+});
